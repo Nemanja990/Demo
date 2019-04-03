95d086e19fc71d0f5aa0dd06dd080165d93fd0bc
Nemanja <nmvrabac@live.com>
2019-04-03 21:39:20 +0200

    Created Product controller. Changed the default route from "Home" to "Product" Controller. Created Json file with dummy data in App_Data folder. Created static class/method "LoadJson.LoadAllProducts" for retreiving the data from json file. Calling the static method from Product controller.

---
+++ Demo/Business/LoadJson.cs
@@ -0,0 +1,33 @@
+﻿using Demo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Business
+{
+    public static class LoadJson
+    {
+        public static List<Product> LoadAllProducts()
+        {
+            List<Product> products = null;
+            try
+            {
+                var path = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/JsonProducts.json");
+                using (StreamReader rdr = new StreamReader(path))
+                {
+                    string json = rdr.ReadToEnd();
+                    products = JsonConvert.DeserializeObject<List<Product>>(json);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return products;
+        }
+    }
+}
---
+++ Demo/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = LoadJson.LoadAllProducts();
+            return View(products);
+        }
+    }
+}
