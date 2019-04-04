1bea986181faac9a0818d977104f0aa6b7805dae
Nemanja <nmvrabac@live.com>
2019-04-04 19:56:06 +0200

    DatabaseProduct Controller, DataContext class using EF Added connection string to web config file

---
+++ Demo/Controllers/DatabaseProductController.cs
@@ -0,0 +1,26 @@
+﻿using Demo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class DatabaseProductController : Controller
+    {
+        private readonly DataContext _context;
+
+        public DatabaseProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DatabaseProduct
+        public ActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
---
+++ Demo/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("Connection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
